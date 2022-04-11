7073a45658ebff3c7ec8adbed7a5a329bf714ff3
a-velazquez <a.m.velazquez@wustl.edu>
2022-04-11 17:20:04 -0500

    testing all 3 functions

--- Exam/easyPoisDevelopment.R
+++ Exam/easyPoisDevelopment.R
@@ -24,4 +24,29 @@ test_fun2 <- function(y, lambda){
 
 test_fun2(vec, test_fun1(vec))
 
+test_fun3 <- function(y, SEtype, B=10){
+  n <- length(y)
+  if(SEtype=="basic"){
+    se_result <- sqrt(test_fun1(y)/n)
+  }
+  else if(SEtype=="bootstrap"){
+    
+    all_samples <- matrix(nrow = n, ncol = B)
+
+    for(i in 1:B){
+      sample_col <- sample(y, n, replace = TRUE)
+      all_samples[,i] <- sample_col
+    }
+
+    sample_mles <- apply(all_samples, 2, test_fun1)
+    
+    se_result <- sd(sample_mles)
+  }
+  
+  return(se_result)
+}
+
+test_fun3(vec, "basic", B=1000)
+test_fun3(vec, "bootstrap", B=1000)
+test_fun3(vec, "bootstrap")
 
