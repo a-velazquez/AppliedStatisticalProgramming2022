f3dbd232b6d54fccbcd2e249fbc88ccdde02b233
a-velazquez <a.m.velazquez@wustl.edu>
2022-04-11 17:55:51 -0500

    begin class definition, set up mle file

---
+++ Exam/PoisMLE-class.R
@@ -0,0 +1,34 @@
+#' A fitted MLE Poisson model
+#' 
+#' Objects of class \code{PoisMLE} are created by the \code{estimatePois} function
+#'
+#' 
+#' An object of the class `PoisMLE' has the following slots:
+#' \itemize{
+#' \item \code{y} Original vector of non-negative integer observations.
+#' \item \code{MLE} The maximum likelihood estimator for given observations.
+#' \item \code{LL} The log likelihood calculated from the observed data using the MLE estimator.
+#' \item \code{SE} The standard error for the MLE.
+#' \item \code{SEtype} The method used to calculate the standard error.
+#' }
+#'
+#' @author Alma Velazquez: \email{a.m.velazquez@@wustl.edu}
+#' @aliases PoisMLE-class initialize,estimatePois-method 
+#' @rdname PoisMLE
+#' @export
+setClass(Class="PoisMLE", 
+         representation = representation(
+           y = "numeric",
+           MLE = "numeric",
+           LL = "numeric",
+           SE = "numeric",
+           SEtype = "character"
+         ),
+         prototype = prototype(
+           y = 0,
+           MLE = 0,
+           LL = 0,
+           SE = 0,
+           SEtype = "basic"
+         )
+)
